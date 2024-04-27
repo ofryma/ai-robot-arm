5146b0c081605afa3df06cb06db7ac71ae2f620d
Ofry Makdasy <101065365+ofryma@users.noreply.github.com>
2024-04-27 13:51:38 +0300

    continues movement

--- control_computer/servo_control.py
+++ control_computer/servo_control.py
@@ -1,13 +1,13 @@
 from time import sleep
 from gpiozero import AngularServo
 
-ud_servo = AngularServo(18 , min_pulse_width=0.0006 , max_pulse_width=0.0023)
-lr_servo = AngularServo(17 , min_pulse_width=0.0006 , max_pulse_width=0.0023)
+ud_servo = AngularServo(18 , min_pulse_width=0.0005 , max_pulse_width=0.0023)
+lr_servo = AngularServo(17 , min_pulse_width=0.0005 , max_pulse_width=0.0023)
 
 while True:
-
     for index , servo in enumerate([ud_servo , lr_servo]):
-        for angle in [90 , 0 , -90]: 
-            print(f"Moving servo {index + 1} to angle: {angle}")
+        print(f"Moving servo {index + 1}")
+        for angle in range(-90 , 90 , step = 1):
             servo.angle = angle
-            sleep(2)
+            sleep(0.1)
+        print(f"Finished servo {index+1}")
