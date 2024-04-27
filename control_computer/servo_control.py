9894aee9a0fe55a5a0b0b38bc0b8fb11dd96c033
Ofry Makdasy <101065365+ofryma@users.noreply.github.com>
2024-04-27 13:52:26 +0300

    continues movement

--- control_computer/servo_control.py
+++ control_computer/servo_control.py
@@ -7,7 +7,7 @@ lr_servo = AngularServo(17 , min_pulse_width=0.0005 , max_pulse_width=0.0023)
 while True:
     for index , servo in enumerate([ud_servo , lr_servo]):
         print(f"Moving servo {index + 1}")
-        for angle in range(-90 , 90 , step = 1):
+        for angle in range(-90 , 90):
             servo.angle = angle
             sleep(0.1)
         print(f"Finished servo {index+1}")
