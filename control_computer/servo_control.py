74a9681d8b1f075bcc6460a8bbdc1491c329091f
Ofry Makdasy <101065365+ofryma@users.noreply.github.com>
2024-04-27 13:45:16 +0300

    two servos

--- control_computer/servo_control.py
+++ control_computer/servo_control.py
@@ -1,11 +1,13 @@
 from time import sleep
 from gpiozero import AngularServo
 
-servo = AngularServo(18 , min_pulse_width=0.0006 , max_pulse_width=0.0023)
+ud_servo = AngularServo(18 , min_pulse_width=0.0006 , max_pulse_width=0.0023)
+lr_servo = AngularServo(17 , min_pulse_width=0.0006 , max_pulse_width=0.0023)
 
 while True:
 
-    for angle in [90 , 0 , -90]: 
-        print(f"Moving to angle: {angle}")
-        servo.angle = angle
-        sleep(2)
+    for index , servo in enumerate([ud_servo , lr_servo]):
+        for angle in [90 , 0 , -90]: 
+            print(f"Moving servo {index + 1} to angle: {angle}")
+            servo.angle = angle
+            sleep(2)
