49b5f614c8ffcb61579330da77fb079addf0297f
Ofry Makdasy <101065365+ofryma@users.noreply.github.com>
2024-04-27 14:21:41 +0300

    test

--- control_computer/servo_control.py
+++ control_computer/servo_control.py
@@ -1,13 +1,22 @@
 from time import sleep
 from gpiozero import AngularServo
 
-ud_servo = AngularServo(18 , min_pulse_width=0.0005 , max_pulse_width=0.0023)
-lr_servo = AngularServo(17 , min_pulse_width=0.0005 , max_pulse_width=0.0023)
+ud_servo = AngularServo(18 , min_pulse_width=0.0006 , max_pulse_width=0.0022)
+lr_servo = AngularServo(17 , min_pulse_width=0.0006 , max_pulse_width=0.0022)
 
 while True:
+
     for index , servo in enumerate([ud_servo , lr_servo]):
-        print(f"Moving servo {index + 1}")
-        for angle in range(-90 , 90):
-            servo.angle = angle
-            sleep(0.1)
-        print(f"Finished servo {index+1}")
+        
+        print(f"Moving servo {index + 1} to min")
+        servo.min()
+        sleep(2)
+        print(f"Moving servo {index + 1} to mid")
+        servo.mid()
+        sleep(2)
+        print(f"Moving servo {index + 1} to max")
+        servo.max()
+        sleep(2)
+        for i in range(-100 , 101):    
+            servo.value = i / 100
+
